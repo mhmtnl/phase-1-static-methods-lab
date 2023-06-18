ce1bba93ae500d309021154f801422e314cdb542
mehmetunlu <munlu4037@gmail.com>
2023-06-18 19:47:40 +0300

    Comppleted assignment

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    const letter = string.slice(0, 1)
+    return letter.toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9\-\'\s]/g, "")
+  }
+
+  static titleize(input) {
+    const exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+
+
+    const words = input.toLowerCase().split(" ");
+
+
+    words[0] = capitalizeWord(words[0]);
+
+
+    for (let i = 1; i < words.length; i++) {
+      if (!exceptions.includes(words[i])) {
+        words[i] = capitalizeWord(words[i]);
+      }
+    }
+
+    const titleized = words.join(" ");
+
+    return titleized;
+  }
+}
+
+
+function capitalizeWord(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
 }
